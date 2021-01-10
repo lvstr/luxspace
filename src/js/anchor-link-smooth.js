873315f1faa69635b4c0cb095cd967a89c3d9a3a
lvstr <serankucing@gmail.com>
2021-01-10 17:20:57 +0700

    update

--- src/js/anchor-link-smooth.js
+++ src/js/anchor-link-smooth.js
@@ -3,7 +3,7 @@ const smoothScrollAnchor = document.querySelectorAll("a[href^='#']");
 for (let index = 0; index < smoothScrollAnchor.length; index++) {
   const el = smoothScrollAnchor[index];
 
-  el.addEventListener("click", (ev) => {
+  el.addEventListener("click", function (ev) {
     ev.preventDefault();
 
     if (document.getElementById(this.getAttribute("href").replace("#", "")))
